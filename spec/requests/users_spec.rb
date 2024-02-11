22b7966b5dae7d66ac69a3d40b480740406565a1
Paul Theby <paultheby@gmail.com>
2024-02-11 15:52:34 -0600

    pausing at Creating sessions controller
    test not succeeding after many tries   stepping back

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Users", type: :request do
   describe 'POST #create' do
   context 'with valid attributes' do
     it 'creates a new user and returns a success response' do
-      post :create, params: { user: attributes_for(:user) }
+      post '/users', params: { user: attributes_for(:user) }
       expect(response).to have_http_status(:created)
       expect(User.count).to eq(1)
     end
@@ -12,10 +12,10 @@ RSpec.describe "Users", type: :request do
 
   context 'with invalid attributes' do
     it 'does not create a new user and returns an error response' do
-      post :create, params: { user: attributes_for(:user, username: nil) }
+      post '/users', params: { user: attributes_for(:user, username: nil) }
       expect(response).to have_http_status(:unprocessable_entity)
       expect(User.count).to eq(0)
     end
   end
 end
-end 
+end
